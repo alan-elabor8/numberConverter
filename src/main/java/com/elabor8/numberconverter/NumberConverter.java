45616d691f44e3b3af1b05a9eeb7bbdce9289397
Alan Mangroo <alan.mangroo@elabor8.com.au>
2018-04-03 15:50:29 +1000

    Added test coverage

--- src/main/java/com/elabor8/numberconverter/NumberConverter.java
+++ src/main/java/com/elabor8/numberconverter/NumberConverter.java
@@ -2,7 +2,13 @@ package com.elabor8.numberconverter;
 
 public class NumberConverter {
 
-    private static String[] unitStrings = {"zero", "one", "two"};
+    private NumberConverter() {}
+
+    private static String[] unitStrings = {
+            "zero",
+            "one",
+            "two"
+    };
 
     public static String convert(int numberToConvert) {
         return unitStrings[numberToConvert];
